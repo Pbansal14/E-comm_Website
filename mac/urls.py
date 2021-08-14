adff2489a36c218b9f2d15d170b75e7ef7ddc893
Jarvis_Keshav <keshavgupta79r@gmail.com>
2021-08-15 03:18:42 +0530

    git commit -a -m 'My commit comments'

--- mac/urls.py
+++ mac/urls.py
@@ -18,12 +18,18 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
+
 from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     path('', views.index)
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
